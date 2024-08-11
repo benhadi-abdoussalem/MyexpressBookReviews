fa3ae52cdc5728ab8cfd0619bce10cca90f64283
benhadi-abdoussalem <dr.programme@gmail.com>
2024-08-11 22:54:22 +0100

    Implementation of the registration process

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,34 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Implementation of the registration process
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+public_users.post("/register", (req,res) => {
+  //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+});
+
 
 public_users.post("/register", (req,res) => {
   //Write your code here
